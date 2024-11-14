2d45758752ee351055e2c0554820b4d4ea16fd69
Sabrina Belloni <sabrina82belloni@gmail.com>
2024-11-14 14:53:31 +0000

    add some js const

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+const API_KEY = "mQR8hTCXcTfwTeqYnKheB-op4jA";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
